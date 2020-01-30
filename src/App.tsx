63b481e32af6ba9a421e1937a32594cb23543a2c
Liz Tiller <elizabeth.tiller@gmail.com>
2020-01-30 13:46:28 -0500

    start

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import Swatch from './components/Swatch';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Typescript Color Picker</h1>
+      </header>
+      <Swatch />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Swatch.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Swatch = () => {
+    return (
+        <h1>Test</h1>
+    )
+}
+
+export default Swatch;
